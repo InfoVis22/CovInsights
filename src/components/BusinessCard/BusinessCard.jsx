02928bad85bb2ee728536861269ba2ebfecf5b78
Maximilian-Brandmaier <kontakt@brandmaier-webentwicklung.de>
2022-12-07 19:49:48 +0100

    Structure and Design of the About Us Page

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -6,22 +6,15 @@ const BusinessCard = ({ name, infoText, imageName }) => {
     // RENDER
 
     return (
-        <div className="Visual">
-            <table>
-                <tr>
-                    <td>
-                        <img className="Avatar"
-                            src={`/images/${imageName}`}
-                            alt={`Image of ${name}`}/>
-                    </td>
-                    <td>
-                        <div className="Heading">
-                            <h2>{name}</h2>
-                            <p>{infoText}</p>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+        <div className="BusinessCard">
+                <img className="Avatar"
+                    src={`/images/${imageName}`}
+                    alt={`Image of ${name}`}/>
+
+                <div className="Heading">
+                    <h2>{name}</h2>
+                    <p>{infoText}</p>
+                </div>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import BusinessCard from '../components/BusinessCard/BusinessCard'
+import './About.scss'
 
 const About = () => {
   return (
-    <div>
+    <div className='Page About'>
+    <h1>Unser Team</h1>
+      <div className="Cards">
         <BusinessCard name={"Manuel Totzauer"}
-        infoText={"In meiner Freizeit reise ich gerne, spiele Schach und Akkordeon."}
-        imageName={"manuelAvatar.jpeg"}/>
-        <BusinessCard name={"Sofie Henghuber"}
-        infoText={"Ich bin Sofie, 27 Jahre alt und studiere Informatik im Master. Mein Schwerpunkt liegt im Quantencomputing und Machine Learning. In meiner Freizeit studiere ich nebenbei noch Schauspiel."}
-        imageName={"sofieAvatar.jpeg"}/>
-        <BusinessCard name={"Lennard Greve"}
-        infoText={"Ich bin Lennard, 24, studiere den Master of Management and Digital Technologies an der LMU.\nFun Fact: Ich habe vor vielen Jahren einen der größten europäischen Minecraft Server gegründet."}
-        imageName={"lennardAvatar.jpeg"}/>
-        <BusinessCard name={"Maximilian Brandmaier"}
-        infoText={"Fun Fact: Max studiert MMT an der LMU München, vor seiner akademisch Karriere ist er mit seiner Tanz-Crew auf verschiedene HipHop Turniere in ganz Deutschland gegangen."}
-        imageName={"maxiAvatar.jpeg"}/>
-        <BusinessCard name={"Alexander Welling"}
-        infoText={"Ich bin Alex und studiere Informatik an der LMU München im Master. In meiner Freizeit schreibe ich gerne About Pages und Masterarbeiten"}
-        imageName={"alexAvatar.jpg"}/>
+          infoText={"In meiner Freizeit reise ich gerne, spiele Schach und Akkordeon."}
+          imageName={"manuelAvatar.jpeg"}/>
+          <BusinessCard name={"Sofie Henghuber"}
+          infoText={"Ich bin Sofie, 27 Jahre alt und studiere Informatik im Master. Mein Schwerpunkt liegt im Quantencomputing und Machine Learning. In meiner Freizeit studiere ich nebenbei noch Schauspiel."}
+          imageName={"sofieAvatar.jpeg"}/>
+          <BusinessCard name={"Lennard Greve"}
+          infoText={"Ich bin Lennard, 24, studiere den Master of Management and Digital Technologies an der LMU.\nFun Fact: Ich habe vor vielen Jahren einen der größten europäischen Minecraft Server gegründet."}
+          imageName={"lennardAvatar.jpeg"}/>
+          <BusinessCard name={"Max Brandmaier"}
+          infoText={"Ich studiere MMT an der LMU München. Vor meiner akademisch Karriere bin ich mit meiner Tanz-Crew auf verschiedene HipHop Turniere in ganz Deutschland gegangen."}
+          imageName={"maxiAvatar.jpeg"}/>
+          <BusinessCard name={"Alexander Welling"}
+          infoText={"Ich bin Alex und studiere Informatik an der LMU München im Master. In meiner Freizeit schreibe ich gerne About Pages und Masterarbeiten"}
+          imageName={"alexAvatar.jpg"}/>
+      </div>
     </div>
   )
 }
