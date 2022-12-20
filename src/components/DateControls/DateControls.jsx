ac13c9e703de4ceae78c7f73e035a1295fb92d68
Maximilian-Brandmaier <kontakt@brandmaier-webentwicklung.de>
2022-12-20 10:29:25 +0100

    Implemented overly complex intervall functions that work in React and dont fuck up the render

--- src/components/DateControls/DateControls.jsx
+++ src/components/DateControls/DateControls.jsx
@@ -1,37 +1,53 @@
 import './DateControls.scss'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 const DateControls = (props) => {
     
- 
-    const [playInterval, setplayInterval] = useState()
+    let timer
+    const [count, setCount] = useState(0)
+    const [isPlaying, setPlaying] = useState(false)
+    
+    const updateCount = () => {
+      timer = !timer && setInterval(() => {
+        setCount(prevCount => prevCount + 1)
+      }, 1000)
 
-    const handleRest = () => {
-        const d = new Date("2018-01-01");
-        props.setSelectedDate(d)
+      //Limit of five years in days
+      if (count === 1835) clearInterval(timer)
     }
+    
+    useEffect(() => {
+        if(isPlaying){
+            updateCount()
+
+            //Here is the magic, adding 1 day every second
+            let newDate = new Date(props.selectedDate);
+            newDate.setDate(newDate.getDate() + 1);
+            props.setSelectedDate(newDate);
+
+            return () => clearInterval(timer)
+        }
+    }, [isPlaying,count])
 
     const handlePlay = () => {
-        const id = setInterval(addADay, 1000);
-        setplayInterval(id)
-    }
-
-    const addADay = () => {
-        //Todo: Find out why this does only work once
-        let newdate = new Date(props.selectedDate);
-        newdate.setDate(newdate.getDate() + 1);
-        props.setSelectedDate(newdate);
+        setPlaying(true);
     }
 
     const handlePause = () => {
-        clearInterval(playInterval);
+        setPlaying(false);
+    }
+
+    const handleReset = () =>{
+        const resetDate = new Date("2018-01-01")
+        props.setSelectedDate(resetDate)
     }
     return (
         <div className="DateControls">
                 <p>Date: {props.selectedDate.toString()}</p>
                 <button onClick={() => handlePlay()}>Play</button>
                 <button  onClick={() => handlePause()}>Pause</button>
-                <button  onClick={() => handleRest()}>Rest</button>
+                <button  onClick={() => handleReset()}>Reset</button>
+
         </div>
     )
 }
