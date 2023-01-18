5e2ec97149f20f284cd916eed79b4b881955160c
AlexanderWelling <alexander.welling@yahoo.de>
2023-01-18 20:49:23 +0100

    improved resources

--- src/components/SourceSpec/SourceSpec.jsx
+++ src/components/SourceSpec/SourceSpec.jsx
@@ -3,12 +3,16 @@ import './SourceSpec.scss'
 const SourceSpec = ({ sourceDescription, sourceLink, lastTimeVisited }) => {
     return (
         <tr className="SourceSpec">
-            <th><li></li></th>
-            <th>{sourceDescription}</th>
             <th>
+                <li></li>
+            </th>
+            <th>
+                <p>{lastTimeVisited}</p>
+            </th>
+            <th>
+                <p>{sourceDescription}</p>
                 <a href={sourceLink}>{sourceLink}</a>
             </th>
-            <th>{lastTimeVisited}</th>
         </tr>
     )
 }
