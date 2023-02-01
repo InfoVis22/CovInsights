f734d0589022e7bc5dbf3c28a6b251ce7b6acda0
Lennard Greve <mail@lgreve.de>
2023-02-01 14:22:28 +0100

    Update Sources

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,19 +25,46 @@ const About = () => {
           imageName={"alexAvatar.jpg"} />
       </div>
 
-      <div id="gant">
-        <img src="/images/Gant.png" alt="Gant Chart" />
-      </div>
-
-      <div className="SourceSpecifications">
+      <div className="Sources">
         <h1>Sources</h1>
-        <table>
-          <SourceSpec sourceDescription={"Insolvenzen im Gastgewerbe in Deutschland gesamt"} sourceLink={"https://www-genesis.destatis.de/genesis//online?operation=table&code=52411-0004&bypass=true&levelindex=0&levelid=1671466031511#abreadcrumb"} lastTimeVisited={"06.01.2023"} />
-          <SourceSpec sourceDescription={"Insolvenzen im Gastgewerbe in Deutschland nach Subbranchen"} sourceLink={"https://www.statistischebibliothek.de/mir/receive/DESerie_mods_00000215"} lastTimeVisited={"06.01.2023"} />
-          <SourceSpec sourceDescription={"Corona Inzidenzen in Deutschland seit 2020"} sourceLink={"https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Inzidenz-Tabellen.html"} lastTimeVisited={"06.01.2023"} />
-          <SourceSpec sourceDescription={"Betriebs- und volkswirtschaftliche Kennzahlen im Gastgewerbe in Deutschland"} sourceLink={"https://www-genesis.destatis.de/genesis//online?operation=table&code=45342-0001&bypass=true&levelindex=0&levelid=1667389597319#abreadcrumb"} lastTimeVisited={"06.01.2023"} />
-          <SourceSpec sourceDescription={"Kurzarbeit im Gastgewerbe in Deutschland"} sourceLink={"https://statistik.arbeitsagentur.de/SiteGlobals/Forms/Suche/Einzelheftsuche_Formular.html;jsessionid=C4ABF408301BFF733C01B9D8D9DFDEFF?gtp=15084_list%253D8&topic_f=kurzarbeit-endg"} lastTimeVisited={"06.01.2023"} />
-        </table>
+
+        <div className='sourcesList'>
+          <h2>Statistisches Bundesamt</h2>
+          <p>45342-0001: Unternehmen, Beschäftigte, Umsatz und weitere betriebs- und
+            volkswirtschaftliche Kennzahlen im Gastgewerbe</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://www-genesis.destatis.de/genesis//online?operation=table&code=45342-0001&bypass=true&levelindex=0&levelid=1675257245756#abreadcrumb' target="_blank">Link</a></p>
+
+          <p>45213-0002: Beschäftigte im Gastgewerbe, Deutschland, Monate, Wirtschaftszweige</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://www-genesis.destatis.de/genesis//online?operation=table&code=45213-0002&bypass=true&levelindex=1&levelid=1675257314387#abreadcrumb' target="_blank">Link</a></p>
+
+          <p>45213-0004: Umsatz im Gastgewerbe: Deutschland, Monate, Wirtschaftszweige</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://www-genesis.destatis.de/genesis//online?operation=table&code=45213-0004&bypass=true&levelindex=1&levelid=1675257314387#abreadcrumb' target="_blank">Link</a></p>
+
+          <p>52411-0004: Insolvenzen im Gastgewerbe in Deutschland gesamt</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://www-genesis.destatis.de/genesis//online?operation=table&code=52411-0004&bypass=true&levelindex=0&levelid=1671466031511#abreadcrumb' target="_blank">Link</a></p>
+
+
+          <h2>Statistische Ämter des Bundes und der Länder</h2>
+          <p>Insolvenzen im Gastgewerbe in Deutschland nach Subbranchen</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://www.statistischebibliothek.de/mir/receive/DESerie_mods_00000215' target="_blank">Link</a></p>
+
+
+          <h2>Robert Koch-Institut</h2>
+          <p>Corona 7-Tage Inzidenzen in Deutschland</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://github.com/robert-koch-institut/COVID-19_7-Tage-Inzidenz_in_Deutschland/blob/main/COVID-19-Faelle_7-Tage-Inzidenz_Deutschland.csv' target="_blank">Link</a></p>
+
+
+          <h2>Bundesagentur für Arbeit</h2>
+          <p>Kurzarbeit im Gastgewerbe in Deutschland</p>
+          <p className='lastVisit'>Abgerufen am: 06.01.2023, <a href='https://statistik.arbeitsagentur.de/SiteGlobals/Forms/Suche/Einzelheftsuche_Formular.html;jsessionid=C4ABF408301BFF733C01B9D8D9DFDEFF?gtp=15084_list%253D8&topic_f=kurzarbeit-endg' target="_blank">Link</a></p>
+
+        </div>
+
+
+        <div id="gant">
+          <img src="/images/Gant.png" alt="Gant Chart" />
+        </div>
+
       </div>
     </div>
   )
